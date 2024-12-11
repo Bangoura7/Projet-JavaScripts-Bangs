0bc93062d02ba26fc2e890b964dce37b0b25e87c
Bangoura7 <bangsabdallah4@gmail.com>
2024-12-11 15:38:31 +0000

    mon premier projet javaScript intégration continu CI

--- section 1/index.js
+++ section 1/index.js
@@ -1,13 +1,27 @@
 
 let contEl = document.getElementById("count")
-console.log(contEl)
-
+let saveEl = document.getElementById("save-el") 
 let count = 0
 
 function increment(){
     count +=1 
     contEl.innerText = count
+
 }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr 
+    count = 0
+    contEl.textContent = count
+}
+
+
+
+
+
+
+
 
 
 
